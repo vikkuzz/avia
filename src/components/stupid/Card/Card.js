79f95dbd669d4495929f66b92979fe6ec0aeea50
Viktor <simpat007@yandex.ru>
2021-01-27 23:37:26 +0300

    card

--- src/components/stupid/Card/Card.js
+++ src/components/stupid/Card/Card.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 import './Card.scss';
 
-const Card = () => <div className="card">this is card</div>;
+const Card = () => (
+  <div className="card">
+    <div className="card__price">
+      13999
+      <img alt="logo" src="S7Logo.png" />
+    </div>
+    <div className="card__info">
+      <span>MOW – HKT</span>
+    </div>
+  </div>
+);
 
 export default Card;
