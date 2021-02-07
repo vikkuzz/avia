50837a6fad7e87fef1d60183e833c7d526be92e1
Viktor <simpat007@yandex.ru>
2021-02-07 16:05:07 +0300

    filter

--- src/components/smart/Filter/Filter.js
+++ src/components/smart/Filter/Filter.js
@@ -1,40 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, connect } from 'react-redux';
 
-import { transfers } from '../../../redux/actions';
+import { dispatchAction } from '../../../redux/actions';
 
 import './Filter.scss';
 
-const Filter = ({ transfer }) => {
+const Filter = (state) => {
   const dispatch = useDispatch();
+  const elements = document.querySelectorAll('label > input');
+  console.log(state);
 
-  if (transfer === 'all') {
-    const elements = document.querySelectorAll('label > input');
-    for (const elem of elements) {
-      elem.checked = true;
+  const checkState = (states, element) => {
+    for (const elem of element) {
+      const { value } = elem;
+      if (states[value]) {
+        elem.checked = true;
+      } else if (!state[value]) {
+        elem.checked = false;
+      }
     }
-  }
+  };
+
+  useEffect(() => {
+    checkState(state, elements);
+  });
 
   return (
     <div className="filter">
       <span className="filter__header">Количество пересадок</span>
       <div className="filter__inputs">
         <label className="filter__item" id="all">
-          <input type="checkbox" value="all" onClick={(e) => dispatch(transfers(e.target.value))} />
+          <input type="checkbox" value="all" onClick={(e) => dispatch(dispatchAction(e.target.value))} />
           Все
         </label>
         <label className="filter__item" id="without">
-          <input type="checkbox" />
+          <input type="checkbox" value="without" onClick={(e) => dispatch(dispatchAction(e.target.value))} />
           Без пересадок
         </label>
         <label className="filter__item" id="one">
-          <input type="checkbox" />1 пересадка
+          <input type="checkbox" value="one" onClick={(e) => dispatch(dispatchAction(e.target.value))} />1 пересадка
         </label>
         <label className="filter__item" id="two">
-          <input type="checkbox" />2 пересадки
+          <input type="checkbox" value="two" onClick={(e) => dispatch(dispatchAction(e.target.value))} />2 пересадки
         </label>
         <label className="filter__item" id="three">
-          <input type="checkbox" />3 пересадки
+          <input type="checkbox" value="three" onClick={(e) => dispatch(dispatchAction(e.target.value))} />3 пересадки
         </label>
       </div>
     </div>
@@ -46,7 +56,11 @@ const mapStateToprops = (state) => ({
   getTickets: state.tickets,
   cheaply: state.cheaply,
   faster: state.faster,
-  transfer: state.transfer,
+  all: state.all,
+  without: state.without,
+  one: state.one,
+  two: state.two,
+  three: state.three,
 });
 
 export default connect(mapStateToprops)(Filter);
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,6 +1,19 @@
 /* eslint-disable no-param-reassign */
 
-const reducer = (state = { count: 0, tickets: [], cheaply: true, faster: false, transfer: null }, action) => {
+const reducer = (
+  state = {
+    count: 0,
+    tickets: [],
+    cheaply: true,
+    faster: false,
+    all: false,
+    one: false,
+    two: false,
+    three: false,
+    without: false,
+  },
+  action
+) => {
   switch (action.type) {
     case 'getTickets':
       state.tickets = action.tickets;
@@ -16,9 +29,63 @@ const reducer = (state = { count: 0, tickets: [], cheaply: true, faster: false, 
     case 'faster':
       return { ...state, faster: true, cheaply: false };
 
-    case 'transfer':
-      state.transfer = action.trans;
+    case 'allTrans':
+      if (state.all && action.transfer === 'all') {
+        state.all = false;
+        state.without = false;
+        state.one = false;
+        state.two = false;
+        state.three = false;
+      } else if (!state.all && action.transfer === 'all') {
+        state.all = true;
+        state.without = true;
+        state.one = true;
+        state.two = true;
+        state.three = true;
+      }
+
+      if (state.without && action.transfer === 'without') {
+        state.without = false;
+        state.all = false;
+      } else if (!state.without && action.transfer === 'without') {
+        state.without = true;
+        if (state.one && state.two && state.three) {
+          state.all = true;
+        }
+      }
+
+      if (state.one && action.transfer === 'one') {
+        state.one = false;
+        state.all = false;
+      } else if (!state.one && action.transfer === 'one') {
+        state.one = true;
+        if (state.without && state.two && state.three) {
+          state.all = true;
+        }
+      }
+
+      if (state.two && action.transfer === 'two') {
+        state.two = false;
+        state.all = false;
+      } else if (!state.two && action.transfer === 'two') {
+        state.two = true;
+        if (state.without && state.one && state.three) {
+          state.all = true;
+        }
+      }
+
+      if (state.three && action.transfer === 'three') {
+        state.three = false;
+        state.all = false;
+      } else if (!state.three && action.transfer === 'three') {
+        state.three = true;
+        if (state.without && state.one && state.two) {
+          state.all = true;
+        }
+      }
+
       return { ...state };
+
     default:
       return state;
   }
